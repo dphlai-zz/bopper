582a905a59b2d5ed61772ce66618c70f74019493
Danny Lai <dannyphlai@gmail.com>
2020-08-31 20:01:19 +1000

    added user/session controllers and templates, secure pw to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
   def new
-  end
+    @user = User.new
+  end # new
 
   def create
-  end
+    @user = User.create user_params
+
+    if @user.persisted?
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end # create
 
   def edit
-  end
+    redirect_to edit_user_path
+  end # edit
 
   def update
-  end
+  end # update
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end #user_params
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :maps
   has_many :scores
+
+  has_secure_password
+
+  validates :name, length: { minimum: 2, too_short: 'not long enough!'}
+  validates :email, presence: true, uniqueness: true
 end
