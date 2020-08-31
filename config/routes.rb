24ddaaf0b05719749e5d7bb12dfe0bf20dedef97
Danny Lai <dannyphlai@gmail.com>
2020-08-31 20:46:45 +1000

    Added login and root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
+  root to: 'pages#home'
+
+  get '/login' => 'session#new'
+
+  post '/login' => 'session#create'
+
+  delete '/login' => 'session#destroy'
+
   resources :users
+
   resources :maps
 
 end
