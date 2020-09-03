5d21390e7ab1f7e16b015f77cdca2eaf7607a7a4
Danny Lai <dannyphlai@gmail.com>
2020-09-03 14:53:05 +1000

    Updated gameover message; tidied up code

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -206,12 +206,13 @@ function collectStar(player, star) {
 }
 
 function onGameover(scene){
-  const GAMEOVER_FEEDBACK_TEXT = "GAMEOVER YOU SUCK! MAYBE CONSIDER NOT SUCKING?"
-  const X_OFFSET = 220;
-  const Y_OFFSET = 20;
-  const CENTER_X = (WIDTH / 2) - (GAMEOVER_FEEDBACK_TEXT.length / 2) - X_OFFSET
-  const CENTER_Y = (HEIGHT / 2) - (GAMEOVER_FEEDBACK_TEXT.length / 2) + Y_OFFSET
-  console.log(this);
+  const GAMEOVER_FEEDBACK_TEXT = "GAMEOVER! 😭"
+  const X_OFFSET = 50;
+  // const Y_OFFSET = 20;
+  // const CENTER_X = (WIDTH / 2) - (GAMEOVER_FEEDBACK_TEXT.length / 2) - X_OFFSET
+  // const CENTER_Y = (HEIGHT / 2) - (GAMEOVER_FEEDBACK_TEXT.length / 2) + Y_OFFSET
+  const CENTER_X = (WIDTH / 2) - X_OFFSET
+  const CENTER_Y = HEIGHT / 2;
   const text = scene.add.text(CENTER_X, CENTER_Y, GAMEOVER_FEEDBACK_TEXT, {
     fontSize: '18px', fill: '#000'
   })
@@ -249,144 +250,63 @@ function generateRandXY(){
     return { x: randomCoordValX, y: randomCoordValY }
 }
 
-// function renderPlatforms(platformLimit, player){
-//   let x = 0, y = 0;
-//   let allRect = []
-//   let isFitted = false, isOverlapped = false;
-//   let h, w
-//   platforms.children.iterate(function(child){
-//     h = child.displayHeight;
-//     w = child.displayWidth;
-//   })
-
-//   for (let currentRow = 0; currentRow < platformLimit; currentRow++) {
-//     x = generateRandXY().x;
-//     y = generateRandXY().y;
-
-//     let rect = new Phaser.Geom.Rectangle(x, y, w, h)
-//     allRect.push(rect)
-
-//     let currentRect = allRect[currentRow];
-//     let nextRect = getNextArrayItem(allRect, currentRow);
-
-//     if (allRect.length > 1){
-//       isOverlapped = Phaser.Geom.Intersects.RectangleToRectangle(currentRect, nextRect)
-//       if (isOverlapped){
-//         for (let currentRect = 0; currentRect < allRect.length; currentRect++) {
-//           x = generateRandXY().x;
-//           y = generateRandXY().y;
-//           rect = new Phaser.Geom.Rectangle(x, y, w, h);
-//           allRect.pop();
-//           allRect.push(rect);
-//         }
-//       }
-//       if ((Math.abs(currentRect.y - nextRect.y)) > player.displayHeight * 2 && !isOverlapped) {
-//         allRect.push(currentRect)
-//       }
-//     }
-//     console.log(isOverlapped);
-//   }// End of rows loop
-//     console.log(allRect);
-//     if (!isOverlapped){
-//       for (let r = 0; r < allRect.length; r++) {
-//         let plat = platforms.create(allRect[r].x, allRect[r].y, 'ground');
-//         plat.displayWidth = Phaser.Math.Between(10, 500);
-//         plat.refreshBody();
-//       }
-//     }
-// }
-
 /*
-const platformRows = []; // all the useable added platforms
-const numberOfRows = 20;
-for(let i=0; i < numberOfRows; i++){
-  1. nothing, sorry
-  2.  useable = false
-  2a. failureCount = 0;
-  3. while !useable
-     3a. platform = random platform with random x,y, random width
-     3b. if platform does NOT overlap with platform on row above
-           (
-            "platform on row above" =
-             you will have to get platformRows[i-1], except not when i=0
-             and ALSO make sure that the element is not null (i.e. nothing on this row)
-           )
-           useable = true
-         end
-         failureCount++;
-         if failureCount > 30  // more than 30? less?
-           break; // break out of loop! too many tries
-         end if
-  (3) end while !useable
-  4. if failureCount <= 30
-        platformRows.push( platform )
-      else
-        // placeholder, so that we always have numberOfRows items in platformRows
-        platformRows.push( null )
-      end
-     // i.e. sometimes you won't add a platform to a row at all!
-} //end for each row
-for each platform in platformRows
-  add it to the map/game (create)
-end
-
 Notes:
   1. Important values!
     1a. The width of the generated rectangle doesnt compare the changing
         platform sprites display width so I need to feed the rectangle instances width
         with those values
     2a. Failure count doesnt seem to do much
-
 */
+
 function renderPlatforms(){
   let platformRows = [];
 
-  const ROW_COUNT = 100;
+  const ROW_COUNT = 10;
   let currentPlatform;
   let previousPlatform;
 
 
   const {dw, dh} = getPlatformDHDW(platforms)
-  console.log(dw, dh)
+
   for (let r = 0; r < ROW_COUNT; r++) {
-    console.log("row = ", r, "****************************");
+
     let failureCount = 0;
     const y = (r + 1) * dh;
     let useable = false;
     while (!useable) {
       const randX = generateRandXY().x;
       const randY = generateRandXY().y;
-      console.log({ randX, y, dw, dh});
-      currentPlatform = new Phaser.Geom.Rectangle(randX, y, dh * failureCount, dh);
-      console.log(currentPlatform, randX, randY)
+
+      currentPlatform = new Phaser.Geom.Rectangle(randX, y, dw, dh);
+
       // Checking for empty data
       previousPlatform = platformRows[r - 1];
 
       if(r > 0 && previousPlatform !== null){
         // If the current platform intersects with the previously generated platform
         // then increment the failure count. If it fails to many times break out of the loop
-        console.log({currentPlatform, previousPlatform});
-        console.log(!Phaser.Geom.Intersects.RectangleToRectangle(currentPlatform, previousPlatform));
+
         if (!Phaser.Geom.Intersects.RectangleToRectangle(currentPlatform, previousPlatform)) {
           useable = true;
         }else{
           failureCount++;
-          console.log({ failureCount })
-          if (failureCount > 100) {
+
+          if (failureCount > 10) {
             break;
           }
         } // closing else for not being useable
       }else{
-        console.log("null break", "r > 0", r > 0, "previousPlatform", previousPlatform === null)
+
         break;
       }
     }
 
     if(platformRows !== undefined){
-      if (failureCount <= 100) {
+      if (failureCount <= 10) {
         platformRows.push(currentPlatform)
       } else {
-        console.log("Pushing null");
+
         platformRows.push(null);
       }
     }
@@ -399,7 +319,7 @@ function renderPlatforms(){
   platformRows.forEach(function (rectangle) {
     let plat;
     if (rectangle != null){
-      console.log({rectangle});
+
       plat = platforms.create(rectangle.x, rectangle.y, 'ground');
     }
     if(plat !== undefined){
